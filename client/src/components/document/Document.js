bdf71dbd837045a66380c180035af440328e6213
vladimir <lovkiy2012@gmail.com>
2020-06-17 18:36:00 +0300

    Document page start

--- client/src/components/document/Document.js
+++ client/src/components/document/Document.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { PDFDownloadLink, Document, Page } from '@react-pdf/renderer'
+import React, {useState, useEffect} from 'react'
+import {connect} from 'react-redux'
+import {getCurrentDevices} from '../../actions/device'
 
-const MyDoc = () => (
-  <Document>
-    <Page>
-      // My document data
-    </Page>
-  </Document>
-)
+const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
+  useEffect(() => {
+    getCurrentDevices()
+  }, [getCurrentDevices])
 
-const Apps = () => (
-  <div>
-    <p>Docuemnt Component</p>
-    {/* <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
-      {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-    </PDFDownloadLink> */}
-  </div>
-)
+  if (!devices.length) {
+    return null
+  }
+  console.log('devices', devices)
 
-export default Apps;
+  const test = () => {
+    console.log('test')
+    let tmpArray = []
+    let objArr = []
+    console.log('objArr', objArr)
+    function itemCheck(item, index) {
+      // console.log('index', index)
+      if (tmpArray.indexOf(item.name) === -1) {
+        tmpArray.push(item.name)
+        objArr.push(item)
+        return
+      }
+      const check = item.check
+      const updatedevice = objArr.find((obj) => obj.name === item.name)
+      updatedevice.check = [...updatedevice.check, ...check]
+      console.log('updatedevice.check', updatedevice.check)
+      console.log('check', check)
+    }
+
+    console.log(
+      'object',
+      devices.forEach((item, index) => {
+        itemCheck(item, index)
+      })
+    )
+  }
+
+  return (
+    <div>
+      <p>Docuemnt Compone</p>
+      <button onClick={test} className="btn btn-dark">
+        test
+      </button>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  devices: state.devices,
+})
+export default connect(mapStateToProps, {getCurrentDevices})(MyDoc)
