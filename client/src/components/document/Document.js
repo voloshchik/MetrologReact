c75af0606604101068b4a8a3b5284b8955ae692c
vladimir <lovkiy2012@gmail.com>
2020-06-18 21:41:35 +0300

    document page table

--- client/src/components/document/Document.js
+++ client/src/components/document/Document.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {getCurrentDevices} from '../../actions/device'
-
+import './document.css'
 const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
   var date = new Date(Date.now()).getFullYear()
 
@@ -95,35 +95,60 @@ const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
       </select>
       <hr />
       {devicesData && (
-        <table>
-          <tbody>
-            <tr>
-              <th rowspan="2">№</th>
-              <th rowspan="2">Название прибора</th>
-              <th rowspan="2">Тип прибора </th>
-              <th rowspan="2">Год поверки</th>
-              <th rowspan="2">К-во сред.</th>
-              <th>К-во поверок по месяцам</th>
-            </tr>
-            <tr>
-              <th>За</th>
-            </tr>
-          </tbody>
-          {devicesData.map((divice, index) => {
-            return (
-              <tbody key={index}>
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>{divice.name}</td>
-                  <td>{divice.type}</td>
-                  <td>{year}</td>
-                  <td>{divice.counter}</td>
-                  <td>{divice.counter}</td>
-                </tr>
-              </tbody>
-            )
-          })}
-        </table>
+        <div className="wrap">
+          <table className="w">
+            <tbody>
+              <tr>
+                <th rowSpan="2">№</th>
+                <th rowSpan="2">Название прибора</th>
+                <th rowSpan="2">Тип прибора </th>
+                <th rowSpan="2">Год поверки</th>
+                <th rowSpan="2">К-во сред.</th>
+                <th colSpan="12">К-во поверок по месяцам</th>
+              </tr>
+              <tr>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+                <th>7</th>
+                <th>8</th>
+                <th>9</th>
+                <th>10</th>
+                <th>11</th>
+                <th>12</th>
+              </tr>
+            </tbody>
+            {devicesData.map((divice, index) => {
+              return (
+                <tbody key={index}>
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{divice.name}</td>
+                    <td>{divice.type}</td>
+                    <td>{year}</td>
+                    <td>{divice.counter}</td>
+
+                    <td>{divice.checkMonths[0]}</td>
+                    <td>{divice.checkMonths[1]}</td>
+                    <td>{divice.checkMonths[2]}</td>
+                    <td>{divice.checkMonths[3]}</td>
+                    <td>{divice.checkMonths[4]}</td>
+                    <td>{divice.checkMonths[5]}</td>
+                    <td>{divice.checkMonths[6]}</td>
+                    <td>{divice.checkMonths[7]}</td>
+                    <td>{divice.checkMonths[8]}</td>
+                    <td>{divice.checkMonths[9]}</td>
+                    <td>{divice.checkMonths[10]}</td>
+                    <td>{divice.checkMonths[11]}</td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+        </div>
       )}
     </div>
   )
