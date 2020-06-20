8812f4c5455a12b68cecdcda9dfc58d15a988b03
vladimir <lovkiy2012@gmail.com>
2020-06-20 23:57:33 +0300

    document page finish

--- client/src/components/document/Document.js
+++ client/src/components/document/Document.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {getCurrentDevices} from '../../actions/device'
 import Axios from 'axios'
 import {saveAs} from 'file-saver'
-
+import './document.css'
 const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
   var date = new Date(Date.now()).getFullYear()
 
@@ -12,24 +12,19 @@ const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
   })
   const [devicesData, setDivicesData] = useState(null)
   const {year} = data
-  const [devicesServer, setDevicesServer] = useState(null)
+
   useEffect(() => {
     console.log('useefect!!!!')
     getCurrentDevices()
   }, [])
-  useEffect(() => {
-    setDevicesServer(devices)
-  }, [devices])
 
-  console.log('devicesServer', devicesServer)
-  console.log('devices', devices)
   const onChange = (e) => {
     setData({...data, [e.target.name]: e.target.value})
   }
-  if (!devices.length) {
-    return null
-  }
 
+  useEffect(() => {
+    test()
+  }, [devices])
   const filterDivicesYear = devices.map((d) => {
     const check = d.check
 
@@ -107,85 +102,83 @@ const MyDoc = ({devices: {devices}, getCurrentDevices}) => {
 
   return (
     <div>
-      <div
-      // className="myForm"
-      >
-        <button onClick={test} className="btn btn-dark"></button>
-        <button className="btn btn-light" onClick={createAndDownloadPdf}>
-          Download PDF
-        </button>
-        <hr />
+      <div className="formDocument">
         <select name="year" value={year} onChange={(e) => onChange(e)}>
           <option>{date}</option>
           <option>{date - 1}</option>
           <option>{date - 2}</option>
         </select>
-        <hr />
+        <button className="btn btn-light" onClick={createAndDownloadPdf}>
+          Download PDF
+        </button>
       </div>
-
+      <hr />
       {devicesData && (
         <div>
           <h1 style={{textAlign: ' center'}}>ПЛАН ГРАФИК</h1>
           <h2 style={{textAlign: ' center'}}>
             Переодической проверки средств измерения на {year} год.
           </h2>
-          <table>
-            <tbody>
-              <tr>
-                <th rowspan="2">№</th>
-                <th rowspan="2">Название прибора</th>
-                <th rowspan="2">Тип прибора </th>
-                <th rowspan="2">Год поверки</th>
-                <th rowspan="2">К-во сред.</th>
-                <th colspan="12">К-во поверок по месяцам</th>
-              </tr>
-              <tr>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>10</th>
-                <th>11</th>
-                <th>12</th>
-              </tr>
-            </tbody>
-            {devicesData.map((divice, index) => {
-              return (
-                <tbody key={index}>
-                  <tr>
-                    <td>{index + 1}</td>
-                    <td>{divice.name}</td>
-                    <td>{divice.type}</td>
-                    <td>{year}</td>
-                    <td>{divice.counter}</td>
+          <div className="center">
+            <table>
+              <tbody>
+                <tr>
+                  <th rowspan="2">№</th>
+                  <th rowspan="2">Название прибора</th>
+                  <th rowspan="2">Тип прибора </th>
+                  <th rowspan="2">Год поверки</th>
+                  <th rowspan="2">К-во сред.</th>
+                  <th colspan="12">К-во поверок по месяцам</th>
+                </tr>
+                <tr>
+                  <th>1</th>
+                  <th>2</th>
+                  <th>3</th>
+                  <th>4</th>
+                  <th>5</th>
+                  <th>6</th>
+                  <th>7</th>
+                  <th>8</th>
+                  <th>9</th>
+                  <th>10</th>
+                  <th>11</th>
+                  <th>12</th>
+                </tr>
+              </tbody>
+              {devicesData.map((divice, index) => {
+                return (
+                  <tbody key={index}>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{divice.name}</td>
+                      <td>{divice.type}</td>
+                      <td>{year}</td>
+                      <td>{divice.counter}</td>
 
-                    <td>{divice.checkMonths[0]}</td>
-                    <td>{divice.checkMonths[1]}</td>
-                    <td>{divice.checkMonths[2]}</td>
-                    <td>{divice.checkMonths[3]}</td>
-                    <td>{divice.checkMonths[4]}</td>
-                    <td>{divice.checkMonths[5]}</td>
-                    <td>{divice.checkMonths[6]}</td>
-                    <td>{divice.checkMonths[7]}</td>
-                    <td>{divice.checkMonths[8]}</td>
-                    <td>{divice.checkMonths[9]}</td>
-                    <td>{divice.checkMonths[10]}</td>
-                    <td>{divice.checkMonths[11]}</td>
-                  </tr>
-                </tbody>
-              )
-            })}
-          </table>
+                      <td>{divice.checkMonths[0]}</td>
+                      <td>{divice.checkMonths[1]}</td>
+                      <td>{divice.checkMonths[2]}</td>
+                      <td>{divice.checkMonths[3]}</td>
+                      <td>{divice.checkMonths[4]}</td>
+                      <td>{divice.checkMonths[5]}</td>
+                      <td>{divice.checkMonths[6]}</td>
+                      <td>{divice.checkMonths[7]}</td>
+                      <td>{divice.checkMonths[8]}</td>
+                      <td>{divice.checkMonths[9]}</td>
+                      <td>{divice.checkMonths[10]}</td>
+                      <td>{divice.checkMonths[11]}</td>
+                    </tr>
+                  </tbody>
+                )
+              })}
+            </table>
+          </div>
           <div
             style={{
               display: ' flex',
-              justifyContent: 'space-between',
+              justifyContent: 'space-around',
               marginTop: '20px',
+              width: '600px',
             }}
           >
             <p>Инженер-инженер метролог</p>
